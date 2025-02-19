5e9723232cab2f359abf5bf56fb1d60c239d7cd5
omarakamal <omaraakamal@gmail.com>
2025-02-19 20:39:51 +0300

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Login from './pages/Login'
 import Homepage from './pages/Homepage'
 import Signup from './pages/Signup'
 import Navbar from './components/Navbar'
+import ValidateIsLoggedIn from './validators/ValidateIsLoggedIn'
+import ValidateIsLoggedOut from './validators/ValidateIsLoggedOut'
 
 function App() {
 
@@ -12,9 +14,9 @@ function App() {
     <>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Homepage/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={<ValidateIsLoggedIn><Homepage/></ValidateIsLoggedIn>}/>
+        <Route path="/signup" element={<ValidateIsLoggedOut><Signup/></ValidateIsLoggedOut>}/>
+        <Route path="/login" element={<ValidateIsLoggedOut><Login/></ValidateIsLoggedOut>}/>
       </Routes>
     </>
   )
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import axios from 'axios'
+import { useNavigate } from "react-router";
 
 // 1. creating the context
 const authContext = createContext()
@@ -8,12 +9,40 @@ function UserProvider(props){
 
     const [user,setUser] = useState(null)
 
+
+    const navigate = useNavigate()
     async function validateToken(){
         // first get the token from localStorage
         const token = localStorage.getItem("token")
-        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/auth/verify`,{headers:{Authorization:`Bearer ${token}`}})
-        console.log(response.data)
-        setUser(response.data)
+
+        if(token){
+        // first condition if there is a valid token
+
+            try{
+
+                const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/auth/verify`,{headers:{Authorization:`Bearer ${token}`}})
+                console.log(response.data)
+                setUser(response.data)    
+            }
+            // second condition if the token is not valid
+            catch(err){
+
+                setUser(null)
+                console.log(err)
+            }
+    
+        }
+        // third condition if there is no token in localstorage
+        else{
+            setUser(null)
+        }
+    }
+
+    function logout(){
+
+        localStorage.removeItem("token")
+        validateToken()
+        navigate("/login")
     }
 
     useEffect(()=>{
@@ -21,7 +50,7 @@ function UserProvider(props){
     },[])
 
 
-    const contextValues = {validateToken,user}
+    const contextValues = {validateToken,user,logout}
     return(
         <authContext.Provider value={contextValues}>
             {props.children}
