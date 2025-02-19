a48d9d171ecca8877ace8bac20b11e6b73795c59
omarakamal <omaraakamal@gmail.com>
2025-02-19 18:37:55 +0300

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import {Routes ,Route} from 'react-router'
-import Login from './components/Login'
+import Login from './pages/Login'
+import Homepage from './pages/Homepage'
+import Signup from './pages/Signup'
 
 function App() {
 
 
   return (
     <>
-  
+
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
 
       
     </>
