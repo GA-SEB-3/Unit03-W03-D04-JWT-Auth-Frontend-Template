d48cc55374bf16f6aa9ee085e52de47184935954
omarakamal <omaraakamal@gmail.com>
2025-02-19 18:44:57 +0300

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,19 @@ import {Routes ,Route} from 'react-router'
 import Login from './pages/Login'
 import Homepage from './pages/Homepage'
 import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
-
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Homepage/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
-
-      
     </>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,7 +3,24 @@ import React from 'react'
 function Signup() {
   return (
     <div>
-      Signup
+      
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input
+         type="text"
+         name='username'
+         id='username'
+          />
+
+        <label htmlFor="password">Password:</label>
+        <input
+         type="password"
+         name='password'
+         id='password'
+          />
+
+          <button>Submit</button>
+      </form>
     </div>
   )
 }
