f919d434dc2552eb082b5aed3fbc1e9fb61e176a
omarakamal <omaraakamal@gmail.com>
2025-02-19 19:03:57 +0300

    updated code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import {useState} from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router'
+
 
 function Login() {
+      const [formData, setFormData] = useState({
+          username:"",
+          password:""
+      })
+
+      const navigate = useNavigate()
+
+      function handleChange(e){
+        setFormData({...formData,[e.target.name]:e.target.value})
+    }
+
+    async function handleSubmit(e){
+      e.preventDefault()
+      try{
+          const token = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/sign-in`,formData)
+          // navigate("/login")
+      }
+      catch(err){
+          console.log(err)
+      }
+  }
+
+
   return (
     <div>
-      Login
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+         type="text"
+         name='username'
+         id='username'
+         value={formData.username}
+         onChange={handleChange}
+          />
+
+        <label htmlFor="password">Password:</label>
+        <input
+         type="password"
+         name='password'
+         id='password'
+         value={formData.password}
+         onChange={handleChange}
+          />
+
+          <button>Submit</button>
+      </form>
     </div>
   )
 }
